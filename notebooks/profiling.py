c8267942606cfeece33edb2a9d82bbf85ac569f1
Miguel Fernandes <miguelrfernandes@users.noreply.github.com>
2025-10-25 12:33:37 +0100

    Refactor profiling module by removing unused imports and consolidating data loading functions

--- notebooks/profiling.py
+++ notebooks/profiling.py
@@ -6,36 +6,33 @@ app = marimo.App(width="medium")
 
 @app.cell
 def __():
-    from pathlib import Path
-    import time
-    from ydata_profiling import ProfileReport
     import pandas as pd
+    return pd,
 
-    url = "https://e-redes.opendatasoft.com/api/explore/v2.1/catalog/datasets/postos_carregamento_ves/exports/csv?lang=pt&timezone=Europe%2FLisbon&use_labels=true&delimiter=%3B"
-    df = pd.read_csv(url, sep=";")
 
+@app.cell
+def __(pd):
+    url_INE = "" # TODO: Add url
+    df_INE = pd.read_csv(url_INE, sep=";")
+    return df_INE, url_INE
+
+
+@app.cell
+def __(pd):
+    url_EREDES = "https://e-redes.opendatasoft.com/api/explore/v2.1/catalog/datasets/postos_carregamento_ves/exports/csv?lang=pt&timezone=Europe%2FLisbon&use_labels=true&delimiter=%3B"
+    df_EREDES = pd.read_csv(url_EREDES, sep=";")
+    return df_EREDES, url_EREDES
+
+
+@app.cell
+def __():
+    from ydata_profiling import ProfileReport
+    # TODO use y_profiling
     dataset_name = "postos_carregamento_ves"
     minimal = True
     mode = "minimal"
     output_path = None
-    return (
-        Path,
-        ProfileReport,
-        dataset_name,
-        df,
-        minimal,
-        mode,
-        output_path,
-        pd,
-        time,
-        url,
-    )
-
-
-@app.cell
-def __(df):
-    df
-    return
+    return ProfileReport, dataset_name, minimal, mode, output_path
 
 
 @app.cell
